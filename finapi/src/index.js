5558b5215b4d6fc68d6101fab9f8ebf09bca96e4
margareth-mee <margoifma@hotmail.com>
2022-05-17 14:19:02 -0300

    metodo post deposito

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,3 +1,5 @@
+const { request } = require('express');
+const { response } = require('express');
 const express = require('express');
 const {v4: uuidv4} = require('uuid'); //v4 gera um uuid randomico
 
@@ -47,6 +49,23 @@ app.post("/account", (request, response) => {
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     const {customer} = request;
     return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const {description, amount} = request.body;
+    
+    const {customer} = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
 });
 
 app.listen(3333);

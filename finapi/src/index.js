0c6802320bdeddda93b993efdc9cf808a0bc08b1
margareth-mee <margoifma@hotmail.com>
2022-05-17 11:52:41 -0300

    metodo post - cadastro de conta

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const {v4: uuidv4} = require('uuid'); //v4 gera um uuid randomico
 
 const app = express();
+app.use(express.json());
 
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement [] 
+ */
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body; //para pegar o cpf e o name que está vindo do request
+    
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+})
 app.listen(3333);
